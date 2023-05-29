f23914fa9b34dd499171986de27b40b3f25d87ce
nttungvn <tungntvn90@gmail.com>
2023-05-29 10:46:32 +0700

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -196,7 +196,7 @@ time_table_insert = ("""
         EXTRACT(YEAR FROM start_time) as year,
         EXTRACT(WEEKDAY FROM start_time) as weekday
     FROM staging_events
-    WHERE e.page = 'NextSong'
+    WHERE staging_events.page = 'NextSong'
     ;
 """)
 
